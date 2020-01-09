51532162dace146069f21744ad6956dfb35bac92
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-01-09 20:11:51 +0000

    Add person class

---
+++ Mechachrome Registration App/Mechachrome Registration App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mechachrome_Registration_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
