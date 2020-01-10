302562235df5269f8d5cb546535c6f46e3d09146
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-01-10 13:11:12 +0000

    Add variables to Person class and getter methods

--- Mechachrome Registration App/Mechachrome Registration App/Person.cs
+++ Mechachrome Registration App/Mechachrome Registration App/Person.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Mechachrome_Registration_App
 {
-    class Person
+    public class Person
     {
+        private string name;
+        private string surname;
+        private string dob;
+        private string address;
+        private string postcode;
+        private string phone;
+        //private string joindate;
+
+        Person(string name, string surname, string dob, string address, string postcode, string phone /*, string joindate*/)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.dob = dob;
+            this.address = address;
+            this.postcode = postcode;
+            this.phone = phone;
+            //this.joindate = joindate;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetSurname()
+        {
+            return surname;
+        }
+        public string GetDob()
+        {
+            return dob;
+        }
+        public string GetAddress()
+        {
+            return address;
+        }
+        public string GetPostcode()
+        {
+            return postcode;
+        }
+
+        public string GetPhone()
+        {
+            return phone;
+        }
     }
 }
--- Mechachrome Registration App/Mechachrome Registration App/Program.cs
+++ Mechachrome Registration App/Mechachrome Registration App/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Mechachrome_Registration_App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Person matt = new Person("Mateusz",
+                                     "Utracki-Janeta",
+                                     "01/01/0001",
+                                     "54 Gap Year",
+                                     "CV5 5GP",
+                                     "7568493923");
+            Console.Write(matt.GetName());
+
         }
     }
 }
